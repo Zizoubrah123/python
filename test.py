a7172ec99a51d7b9d99eb466b8cfc1bd42722054
aziz <johndoe@example.com>
2023-05-15 19:33:09 +0200

    start coding

---
+++ test.py
@@ -0,0 +1,44 @@
+# num = 25
+# dec = 4.2
+
+# print(num)
+# print(type(dec))
+
+# num_to_dec = float(num)
+# import random
+# rand_num = random.randint(2,5)
+
+# print( $)
+
+# int_to_float = float(35)
+# float_to_int = int(44.2)
+# int_to_complex = complex(35)
+# print(int_to_float)
+# print(float_to_int)
+# print(int_to_complex)
+# print(type(int_to_float))
+# print(type(float_to_int))
+# print(type(int_to_complex))
+
+# import random
+# print(random.randint(2,5)) # provides a random number between 2 and 5
+
+# name = "Zen"
+# print("My name is", name)
+
+# name = "Zen"
+# print("My name is " + name)
+
+# x = "hello world"
+# print(x.title())
+# # output:
+# "Hello World"
+
+fruits = ['apple', 'banana', 'orange', 'strawberry']
+vegetables = ['lettuce', 'cucumber', 'carrots']
+fruits_and_vegetables = fruits + vegetables
+print(fruits_and_vegetables)
+salad = 3 * vegetables
+print(salad)
+
+
