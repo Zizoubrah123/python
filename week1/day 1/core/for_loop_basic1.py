935a3dab1ef585e3013a12b6baf4ae36afeaff64
aziz <johndoe@example.com>
2023-05-15 21:58:37 +0200

    aaaa

---
+++ week1/day 1/core/for_loop_basic1.py
@@ -0,0 +1,32 @@
+
+# for i in range (0,151):
+#     print(i)
+
+# for i in range (5,1005,5):
+#     print(i)
+
+
+# ps = "coding"
+# for i in range (0,101):
+
+#     if i%10 == 0 :
+#         print("codingDojo")
+#     elif i%5 == 0 :
+#         print ("coding")
+#     else:
+#         print(i)
+
+# sum = 0
+# for i in range (0, 500001):
+#     if i%2 == 1:
+#         sum = sum + i
+# print(sum)
+
+lowNum = 2
+highNum = 9
+mult = 3
+
+for i in range (lowNum, highNum+1):
+    if i %mult == 0:
+        print(i)
+
