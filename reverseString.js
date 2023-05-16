bc329cbe35186b15dac039c26ed0c575edd89750
aziz <johndoe@example.com>
2023-05-16 20:31:49 +0200

    aaaa

---
+++ reverseString.js
@@ -0,0 +1,32 @@
+// const str1 = "creature";
+
+// function reverseString(str) {
+// var reversed = "";
+
+// for (var i = str.length - 1; i >= 0; i--  ){
+//     reversed += str[i]
+// }
+
+
+
+
+const str1 = "object oriented programming";
+
+
+function stringReverse(str1) {
+    var acronym = "";
+    
+    for (var i= 0; i < str1.length; i++){
+        if (str1[i] = "")
+        str1 = str1[i+1];
+
+
+    
+}
+
+
+
+
+
+
+
