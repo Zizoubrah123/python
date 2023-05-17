9adc04a74e389a956d4d2253d7b3ed2a9d24e5f0
aziz <johndoe@example.com>
2023-05-17 18:46:37 +0200

    aaaa

--- week1/day 2/core/bank account.py
+++ week1/day 2/core/bank account.py
@@ -30,10 +30,33 @@ class BankAccount:
             self.balance = (self.balance * self.int_rate) + self.balance
             return self
 
-azizBarrah = BankAccount(0.01, 0,)
+azizBarrah = BankAccount(0.01, 0,) 
 maramBetaib = BankAccount(0.01, 0,)
 
 azizBarrah.deposit(30).deposit(20).deposit(10).display_account_info().withdraw(50).yield_interest().display_account_info()
 maramBetaib.deposit(100).deposit(50).display_account_info().withdraw(30).withdraw(20).withdraw(10).withdraw(30).yield_interest().display_account_info()
+
+# class User:
+#     def __init__(self, name, email):
+#         self.name = name
+#         self.email = email
+#         self.account = BankAccount(int_rate=0.02, balance=0)
+#     # other methods
+    
+#     def make_deposit(self, amount):
+#         self.account.deposit(amount)
+#         print(self.account.balance)
+#         return self
+
+#     # def make_withdraw(self, amount):
+#     #     self.
+
+# azizBarrah = User("aziz", "barrah@gmail.com")
+
+# azizBarrah.make_deposit()
+
+
+
+
 
 
