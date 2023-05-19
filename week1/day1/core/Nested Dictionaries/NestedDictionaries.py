8b49ff32ad87448fe7b412cc158f69d96cbc1194
aziz <johndoe@example.com>
2023-05-19 13:43:06 +0200

    www

--- week1/day1/core/Nested Dictionaries/NestedDictionaries.py
+++ week1/day1/core/Nested Dictionaries/NestedDictionaries.py
@@ -1,44 +1,47 @@
-# x = [ [5,2,3], [10,8,9] ] 
-# students = [
-#     {'first_name':  'Michael', 'last_name' : 'Jordan'},
-#     {'first_name' : 'John', 'last_name' : 'Rosales'}
-# ]
-# sports_directory = {
-#     'basketball' : ['Kobe', 'Jordan', 'James', 'Curry'],
-#     'soccer' : ['Messi', 'Ronaldo', 'Rooney']
-# }
-# z = [ {'x': 10, 'y': 20} ]
+x = [ [5,2,3], [10,8,9] ] 
+students = [
+    {'first_name':  'Michael', 'last_name' : 'Jordan'},
+    {'first_name' : 'John', 'last_name' : 'Rosales'}
+]
+sports_directory = {
+    'basketball' : ['Kobe', 'Jordan', 'James', 'Curry'],
+    'soccer' : ['Messi', 'Ronaldo', 'Rooney']
+}
+z = [ {'x': 10, 'y': 20} ]
 
-# x[1][0] = 15
-# students[0]['last_name'] = 'Bryant'
-# sports_directory['soccer'][0] = 'andres'
-# z[0]['y'] =30
+x[1][0] = 15
+students[0]['last_name'] = 'Bryant'
+sports_directory['soccer'][0] = 'andres'
+z[0]['y'] =30
 
-# students = [
-#     {'first_name':  'Michael', 'last_name' : 'Jordan'},
-#     {'first_name' : 'John', 'last_name' : 'Rosales'},
-#     {'first_name' : 'Mark', 'last_name' : 'Guillen'},
-#     {'first_name' : 'KB', 'last_name' : 'Tonel'}
-#     ]
+print("===========================")
+
+students = [
+    {'first_name':  'Michael', 'last_name' : 'Jordan'},
+    {'first_name' : 'John', 'last_name' : 'Rosales'},
+    {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+    {'first_name' : 'KB', 'last_name' : 'Tonel'}
+    ]
 
 
-# def iterateDictionary(list):
-#     for i in range(0,len(list)):
-#         for key,vul in list[i].items():
-#             print(f"{key}, {vul},")
+def iterateDictionary(list):
+    for i in range(0,len(list)):
+        for key,vul in list[i].items():
+            print(f"{key}, {vul},")
 
-# def iterateDictionary2(first_name, list) :
-#     for i in range(len(list)):
-#         print(list[i]["first_name"])
+def iterateDictionary2(first_name, list) :
+    for i in range(len(list)):
+        print(list[i]["first_name"])
 
-# def iterateDictionary3(last_name, list) :
-#     for i in range(len(list)):
-#         print(list[i]["last_name"])
+def iterateDictionary3(last_name, list) :
+    for i in range(len(list)):
+        print(list[i]["last_name"])
 
-# iterateDictionary(students)
-# iterateDictionary2(students, students)
-# iterateDictionary3(students, students)
+iterateDictionary(students)
+iterateDictionary2(students, students)
+iterateDictionary3(students, students)
 
+print("============================")
 
 dojo = {
     'locations': ['San Jose', 'Seattle', 'Dallas', 'Chicago', 'Tulsa', 'DC', 'Burbank'],
