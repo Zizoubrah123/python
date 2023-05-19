4acb30932d8ccb5481d6256a8f4a5215479d8fa6
aziz <johndoe@example.com>
2023-05-19 19:59:18 +0200

    lllll

--- week1/day3/practice/basletball.py
+++ week1/day3/practice/basletball.py
@@ -5,57 +5,10 @@ class Player:
         self.position =dictionary["position"] 
         self.team =dictionary["team"] 
 
+    def __repr__(self):
+        display = f"Player: {self.name}, {self.age} y/o, pos: {self.position}, team: {self.team}"
 
-
-players = [
-    {
-    "name": "Kevin Durant", 
-    "age":34, 
-    "position": "small forward", 
-    "team": "Brooklyn Nets"
-    },
-    {
-    "name": "Jason Tatum", 
-    "age":24, 
-    "position": "small forward", 
-    "team": "Boston Celtics"
-    },
-    {
-    "name": "Kyrie Irving", 
-    "age":32,
-        "position": "Point Guard", 
-    "team": "Brooklyn Nets"
-    },
-    {
-    "name": "Damian Lillard", 
-    "age":33,
-    "position": "Point Guard", 
-    "team": "Portland Trailblazers"
-    },
-    {
-    "name": "Joel Embiid", 
-    "age":32,
-        "position": "Power Foward", 
-    "team": "Philidelphia 76ers"
-    },
-    {
-        "name": "DeMar DeRozan",
-        "age": 32,
-        "position": "Shooting Guard",
-        "team": "Chicago Bulls"
-    }
-]
-
-p_Kevin = Player(players[0])
-
-print(p_Kevin.name)
-print("--------------------")
-class Player1:
-    def __init__(self, dictionary):
-        self.name = dictionary["name"]
-        self.age =dictionary["age"] 
-        self.position =dictionary["position"] 
-        self.team =dictionary["team"] 
+        return display
 
 kevin = {
     "name": "Kevin Durant", 
@@ -77,44 +30,10 @@ kyrie = {
 }
 
 
-player_Kevin = Player1(kevin)
-print  (player_Kevin.team)
-print ("---------------------")
+player_Kevin = Player(kevin)
+player_jason = Player(jason)
+player_kyrie = Player(kyrie)
 
-player_jason = Player1(jason)
-print (player_jason.position)
-print ("------------------------")
-player_kyrie = Player1(kyrie)
-print(player_kyrie.age)
-
-print("-------------------------------------------------")
-
-class Player2:
-    def __init__(self, dictionary):
-        self.name = dictionary["name"]
-        self.age =dictionary["age"] 
-        self.position =dictionary["position"] 
-        self.team =dictionary["team"] 
-
-damain = {
-    "name": "Damian Lillard", 
-    "age":33,
-    "position": "Point Guard", 
-    "team": "Portland Trailblazers"
-}
-
-player_damain = Player1(damain)
-print(player_damain.position)
-
-print("--------------------------------------")
-
-class team:
-    def __init__(self, dictionary):
-        self.name = dictionary["name"]
-        self.age =dictionary["age"] 
-        self.position =dictionary["position"] 
-        self.team =dictionary["team"] 
-new_team = []
-for i in range (len(players)):
-    new_team.append(team(players[i]))
-
+print(player_jason)
+print(player_Kevin)
+print(player_kyrie)
