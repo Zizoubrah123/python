9b68773a59b49d6c654676a040d93a245d11b974
aziz <johndoe@example.com>
2023-05-23 17:02:53 +0200

    s

---
+++ week1/day4/practice/playground/server.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/play/<int:num>')
+def hello_world(num):
+    return render_template('index.html', num=num)
+
+# @app.route('/play/<string:banana>/<int:num>')
+# def repeat(banana, num):
+#     return render_template('index.html','style.css', banana=banana, num=num)
+
+if __name__ == "__main__":
+    app.run(debug=True)
